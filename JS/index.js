580baa3a93f45195a456c746a38faf1b6f3b53ca
NoemiB <noemi.bfortes@gmail.com>
2026-02-04 21:22:43 +0100

    Añado modal para buscador de recetas

--- JS/index.js
+++ JS/index.js
@@ -9,6 +9,22 @@ let searchReceta = document
     }
     document.getElementById("searchInput").value = "";
     let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputReceta}`;
+    const modal = document.getElementById("modalReceta");
+    const modalTitle = document.getElementById("modalTitle");
+    const modalInstructions = document.getElementById("Instructions");
+    const closeModal = document.getElementById("closeModal");
+    const modalImage = document.getElementById("modalImage");
+
+    closeModal.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
@@ -33,7 +49,11 @@ let searchReceta = document
           card.appendChild(button);
 
           button.addEventListener("click", () => {
-            alert(receta.strInstructions);
+            modalTitle.textContent = receta.strMeal;
+            modalInstructions.textContent = receta.strInstructions;
+            modalImage.src = receta.strMealThumb;
+            modalImage.alt = receta.strMeal;
+            modal.style.display = "flex";
           });
 
           recetasContenedor.appendChild(card);
